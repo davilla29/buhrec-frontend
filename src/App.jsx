de210072d9ae3a23869ae4452883a96e77224d8a
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-03-04 00:09:14 +0100

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Toaster } from "react-hot-toast";
 import { routes } from "./routes/routes";
+import useCheckAuthInit from "./hooks/auth/useCheckAuthInit";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter(routes);
 
 function App() {
+  useCheckAuthInit();
+
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
