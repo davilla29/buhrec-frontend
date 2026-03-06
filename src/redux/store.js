eab4bb978c82f7477a997ee338d9ca4a45c84a71
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-03-06 10:35:13 +0100

    add assignment slice and update admin assignment page to work with it

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,11 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage
 
 import authReducer from "./auth/authSlice.js";
+import assignmentReducer from "./assignment/assignmentSlice.js";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  assignments: assignmentReducer,
 });
 
 const persistConfig = {
