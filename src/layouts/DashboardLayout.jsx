4fe40299522ae9b7b3d87bbe2521efffbbdb48a0
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-03-09 17:09:07 +0100

    add scroll to top hook to layouts file

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/core/Sidebar";
+import ScrollToTop from "../hooks/ScrollToTop";
 
 const DashboardLayout = () => {
   return (
     <div className="flex h-screen bg-white overflow-hidden">
+      <ScrollToTop />
       <div className="shrink-0">
         <Sidebar />
       </div>
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,10 +2,12 @@ import { lazy, Suspense } from "react";
 const Header = lazy(() => import("../components/Header"));
 const Footer = lazy(() => import("../components/Footer"));
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../hooks/ScrollToTop"
 
 const RootLayout = () => {
   return (
     <div className="min-h-screen bg-gray-50">
+      <ScrollToTop />
       <Header />
 
       <main className="grow">
