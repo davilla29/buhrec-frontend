d3da2e70d6683d1436cd100ba03e6769b5edf11f
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-03-24 18:07:35 +0100

    update scrollToTop component to work in dashboard layout

--- src/hooks/ScrollToTop.jsx
+++ src/hooks/ScrollToTop.jsx
@@ -5,11 +5,22 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
+    const scrollContainer = document.getElementById("dashboard-scroll");
+
+    if (scrollContainer) {
+      scrollContainer.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    } else {
+      // fallback for layouts like RootLayout
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
   }, [pathname]);
 
   return null;
